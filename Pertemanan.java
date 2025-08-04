7d06a83f8a00cd7faf957ed18941c8e4ebf2da9c
REVANTYO <revantyo_prasetyawan_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:47:48 +0700

    Add files via upload

---
+++ Pertemanan.java
@@ -0,0 +1,45 @@
+public class Pertemanan {
+    public static void main(String[] args) {
+
+        //nama teman
+        String nama1= "Daffa aqiilah praditra";
+        String nama2= "Muhammad gavrila rafi prayata";
+        String nama3 = "Wildan hanif";
+        String nama4 = "mohammad ibrahimovic";
+        String nama5= "Fatih lintang adz ahabi";
+
+        //umur
+        int usia1 = 15;
+        int usia2 = 15;
+        int usia3 = 16;
+        int usia4=  15;
+        int usia5=  15;
+
+        //cita cita teman
+         String citacita1 = "Polisi";
+         String citacita2 = "pengusahha";
+         String citacita3 = "Berguna bagi org lain";
+         String citacita4 = "Futsal";
+         String citacita5 = "manager";
+
+         System.out.println("Daffa aqiilah praditra");
+         System.out.println("Muhammad gavrila rafi prayata");
+         System.out.println("Wildan hanif");
+         System.out.println("mohammad ibrahimovic");
+         System.out.println("Fatih lintang adz ahabi");
+    
+         
+
+         System.out.println(15);
+         System.out.println(15);
+         System.out.println(16);
+         System.out.println(15);
+         System.out.println(15);
+
+         System.out.println("polisi");
+         System.out.println("pengusaha");
+         System.out.println("berguna bagi org lain");
+         System.out.println("futsal");
+         System.out.println("manager");
+    }
+}
